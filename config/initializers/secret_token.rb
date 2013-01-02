3712c2d29dfff2b19bceb0879ffe9c1e0a2914af
frika <steve.michael.thomas@gmail.com>
2013-01-02 09:15:30 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MacleansParser::Application.config.secret_token = '92c5c5b32163bdfa5920185428f68f4d0883e7c17743d82b49de1b0771351904769a578f077aa260f62f6b1be1136e4b84b047d2ecbcd2b1a40719e210d5d3dc'
