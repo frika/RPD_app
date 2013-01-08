15af1d91d809e7281e4b6789de21f06c0ffad078
frika <steve.michael.thomas@gmail.com>
2013-01-08 12:41:24 -0500

    fixed things

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -1,4 +1,7 @@
 class Issue < ActiveRecord::Base
   attr_accessible :publication, :name, :asset
   has_attached_file :asset, :default_url => "/public/issue/:name/:name.zip"
+  validates :name, presence: true, uniqueness: true
+  validates :publication, presence: true
+  validates :asset, presence: true
 end
